4a786be162f02f60183ad5714da073a67500a11f
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-05-09 15:14:43 +0200

    prinft start

--- prinft.c
+++ prinft.c
@@ -6,9 +6,15 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 11:23:05 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/05/09 11:26:20 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/05/09 15:13:27 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.a"
+#include "libftprintf.h"
 
+int ft_printf(const char *s, ...)
+{
+	va_list list;
+	va_start (list, s);
+}
