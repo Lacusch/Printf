1d272f0e5f8ab8c9137665b893d8fb8150d473ca
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-05-20 17:35:12 +0200

    Dirty fix to some problems

--- ft_prinft.c
+++ ft_prinft.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 11:23:05 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/05/12 11:15:08 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/05/20 17:34:31 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,8 @@ void	ft_process_str(va_list ap, char c)
 	if (c == 'c')
 		ft_putchar_fd((char)va_arg(ap, int), 1);
 	if (c == 's')
-		ft_putstr_fd(va_arg(ap, char*), 1);
-	if (c == 'p'|| c == 'x'|| c == 'X')
+		ft_putstr_fd(va_arg (ap, char*), 1);
+	if (c == 'p' || c == 'x' || c == 'X')
 	{
 		ft_putstr_fd("0x", 1);
 		ft_put_hex(va_arg(ap, unsigned long), c, 1);
@@ -62,7 +62,9 @@ int	ft_printf(const char *s, ...)
 {
 	va_list	ap;
 	int		i;
+	int		c;
 
+	c = 0;
 	i = 0;
 	va_start (ap, s);
 	while (s[i] != '\0')
@@ -70,14 +72,17 @@ int	ft_printf(const char *s, ...)
 		if (s[i] == '%')
 		{
 			i++;
+			c++;
 			ft_process_str(ap, s[i]);
+			i++;
 		}
 		else
 		{
 			ft_putchar_fd(s[i], 1);
 			i++;
+			c++;
 		}
 	}	
 	va_end(ap);
-	return (i);
+	return (c);
 }
