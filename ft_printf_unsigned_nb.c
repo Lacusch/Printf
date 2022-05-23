1d6a73f992f8252695c293521064dde6959447bb
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-05-23 16:17:49 +0200

    Added unsigned nb / other small fixes

--- ft_printf_unsigned_nb.c
+++ ft_printf_unsigned_nb.c
@@ -6,10 +6,12 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 15:40:33 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/05/23 15:52:23 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/05/23 16:16:05 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft/libft.h"
+#include "libftprintf.h"
 int ft_unsigned_len (unsigned int num)
 {
 	int i;
@@ -32,7 +34,7 @@ int	ft_printf_unsigned_nb(unsigned int nb)
 		ft_putchar_fd(nb + '0', 1);
 	else
 	{
-		ft_put_un_nbr_fd(nb / 10);
+		ft_put_un_nbr_fd(nb / 10, 1);
 		ft_putchar_fd((nb % 10) + 0, 1);
 	}
 	return (ft_unsigned_len(number));
