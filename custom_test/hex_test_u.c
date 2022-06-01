d96f5a9eff7f4649da3bd6f7a2a33f5eaa399f02
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-06-01 17:35:44 +0200

    Testter update

--- custom_test/hex_test_u.c
+++ custom_test/hex_test_u.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 16:51:14 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/06/01 16:51:23 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2022/06/01 17:33:05 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@
 int main (void)
 {
 	int i1, i2;
-	int h;
-	h = 102;
+	long h;
+	h = 140643137509472;
 	printf ("Base printf:\n");
-	i1 = printf("%x", h);
+	i1 = printf("%lx", h);
 	printf("\n");
 	printf ("The return value of printf is %i\n", i1);
 	printf ("My printf:\n");
-	i2 = ft_printf("%x", h);
+	i2 = ft_printf("%lx", h);
 	printf("\n");
 	printf ("The return value of printf is %i\n", i2);
 	return (0);
