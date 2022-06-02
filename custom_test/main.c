1b52e85dc7854bbd1140e0dac826dbe8c57ee668
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-06-02 16:02:26 +0200

    Added basic, quick to change tester

--- custom_test/main.c
+++ custom_test/main.c
@@ -1,72 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: slaszlo- <slaszlo-@student.42heilbronn.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/16 11:11:05 by slaszlo-          #+#    #+#             */
-/*   Updated: 2022/06/01 14:35:04 by slaszlo-         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "../ft_printf.h"
 #include "../libft/libft.h"
 #include "limits.h"
-// int ft_number_digit (unsigned int number);
 
 int main (void)
 {
-	// int i, i2, i3;
-	// i = 178;
-	// i2 = 0;
-	// i3 = 0;
-	// // printf("The number in hex is: ");
-	// i2 = printf("%X", i);
-	// printf("\nThe return value of printf is : %d", i2);
-	// i3 = ft_printf("%X", i);
-	// printf("\nThe return value of My printf is : %d", i3);
-	// ft_printf("%X\n",i);
-	// // printf("%x", i);
-	// // write_hex(i);
-	// // printf("\n%d", ft_count_hex(i));
-	// // ft_printf("My function writes :");
-	// // write_hex(i);
-	// return (1);
-
 	int i1, i2;
-	unsigned int u;
-	u = 987654;
-	printf ("Base printf\n");
-	i1 = printf ("%u", u);
-	printf ("\n");
-	printf ("The return value of printf is %i\n", i1);
-	printf ("My printf\n");
-	i2 = ft_printf ("%u", u);
-	printf ("\nThe return value of My printf is %i\n", i2);
-
-	// int i1, i2;
-	// i1 = ft_printf("%i", i);
-	// i2 = printf("%i", i);
-	// // printf ("The return of ft_printf is:%i\n", i1);
-	// // printf("\nThe return value of My printf is : %i", i1);
-	// printf ("The return of printf is:%i\n", i2);
+	i1 = printf("This %p is even stranger", (void *)-1);
+	printf("\n");
+	i2 = ft_printf("This %p is even stranger", (void *)-1);
+	printf("\n");
+	printf("Pritf returns %d, My printf returns %d", i1, i2);
 	return (0);
 }
-
-// int ft_number_digit (unsigned int number)
-// {
-// 	int i;
-// 	while (number != 0)
-// 	{
-// 		number = number /10;
-// 		i++;
-// 	}
-// 	return (i);
-// }
-// unsigned int u;
-// u = 35798;
-// ft_printf("%d", ft_number_digit(u));
