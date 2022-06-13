f9fe43160df8808b173bde12f40990eb0d3682d5
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2022-06-13 05:32:00 -0700

    Added readme to the stuff to fix / updated test for the next stuff to fix

--- custom_test/main.c
+++ custom_test/main.c
@@ -7,11 +7,16 @@
 
 int main (void)
 {
-	int i1, i2;
-	i1 = printf("This %p is even stranger", (void *)-1);
+	int i1, i2, i3;
+	// printf("%i", ft_count_hex((void *)-1));
+	i1 = printf(" %p %p ", ULONG_MAX, -ULONG_MAX);
 	printf("\n");
-	i2 = ft_printf("This %p is even stranger", (void *)-1);
+	i2 = ft_count_hex(ULONG_MAX);
+	i3 = ft_count_hex(-ULONG_MAX);
+	// i2 = ft_printf(" %p %p ", ULONG_MAX, -ULONG_MAX);
 	printf("\n");
-	printf("Pritf returns %d, My printf returns %d", i1, i2);
+	// printf("Pritf returns %d, My printf returns %d\n", i1, i2);
+	printf("\nPritf returns %d, count hex returns %d an %d", i1, i2, i3);
+	// printf ("ft_number_digit returns: %d", ft_number_digit((unsigned int)4294967295));
 	return (0);
 }
